1180b0ac5e0b0e92233ac5b8a1bc48da02042d41
sunarto <yanu101000@gmail.com>
2024-07-02 11:35:05 +0700

    session 8 grpc

---
+++ session-8-introduction-grpc/client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	pb "solution1/session-8-introduction-grpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	runClient()
+}
+
+func runClient() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	greeterClient := pb.NewGreeterClient(conn)
+
+	name := "world"
+	r, err := greeterClient.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
